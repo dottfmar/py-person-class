10dfb06818becb7db423e40e06e8e8367908aa91
Mariana Lishchynska <lishchynska.m@gmail.com>
2024-11-27 19:29:23 +0200

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -8,14 +8,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person_dict in people:
-        person = Person(person_dict["name"], person_dict["age"])
-        person_list.append(person)
-    for person_dict in people:
-        person = Person.people[person_dict["name"]]
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            person.wife = Person.people[person_dict["wife"]]
-        elif "husband" in person_dict and person_dict["husband"] is not None:
-            person.husband = Person.people[person_dict["husband"]]
-    return person_list
+    persons = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+        if person_data.get("wife"):
+            person.wife = Person.people[person_data["wife"]]
+        elif person_data.get("husband"):
+            person.husband = Person.people[person_data["husband"]]
+    return persons
