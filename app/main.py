f987eca4ce06016e6d2debb9af4503583b5e535b
Mariana Lishchynska <lishchynska.m@gmail.com>
2024-11-28 14:47:45 +0200

    'Changed'

--- app/main.py
+++ app/main.py
@@ -7,15 +7,15 @@ class Person:
         Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
+def create_person_list(people: list[dict]) -> list[Person]:
     persons = [
         Person(person_data["name"], person_data["age"])
         for person_data in people
     ]
-    for person_data in people:
-        person = Person.people[person_data["name"]]
-        if person_data.get("wife"):
-            person.wife = Person.people[person_data["wife"]]
-        elif person_data.get("husband"):
-            person.husband = Person.people[person_data["husband"]]
+    for person_instance in people:
+        person = Person.people[person_instance["name"]]
+        if person_instance.get("wife"):
+            person.wife = Person.people[person_instance["wife"]]
+        elif person_instance.get("husband"):
+            person.husband = Person.people[person_instance["husband"]]
     return persons
